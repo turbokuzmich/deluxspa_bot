0e5daedaf70c6bb6e1008d749c6eb26d30ea7bd1
Kurteev Dmitry <kurteev.d@yandex.ru>
2022-12-16 05:43:14 +0300

    Upload to Yandex Cloud Object Storage

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
+import fs from "fs/promises";
+import aws from "aws-sdk";
 import path from "path";
 import { config } from "dotenv";
 import TelegramBot from "node-telegram-bot-api";
 
 config();
 
+let isBusy = false;
+
 const uploads = {};
 const downloadDir = path.resolve("./downloads");
 const isProduction = process.env.NODE_ENV === "production";
+
+const bucketName = "deluxspa-downloads";
+const catalogKey = "catalog.pdf";
 
 const bot = new TelegramBot(
   process.env.TELEGRAM_API_TOKEN,
   isProduction ? { webHook: true } : { polling: true }
 );
 
+const s3 = new aws.S3({
+  region: process.env.AWS_REGION,
+  endpoint: process.env.AWS_ENDPOINT,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+bot.onText(/^\/link$/, async (msg) => {
+  const { chat } = msg;
+  const { id } = chat;
+
+  await bot.sendMessage(
+    id,
+    `Адрес каталога — https://${bucketName}.storage.yandexcloud.net/${catalogKey}`
+  );
+});
+
 bot.onText(/^\/upload$/, async (msg) => {
   const { chat } = msg;
   const { id } = chat;
 
-  if (uploads[id]) {
+  if (isBusy) {
+    await bot.sendMessage(id, "В данный момент я уже загружаю каталог");
+  } else if (uploads[id]) {
     await bot.sendMessage(id, "Я все еще жду файла");
   } else {
     uploads[id] = {
@@ -42,16 +70,48 @@ bot.on("document", async (msg) => {
     return bot.sendMessage(id, "Нужно прислать именно pdf");
   }
 
-  const uploadPath = await bot.downloadFile(file_id, downloadDir);
+  isBusy = true;
 
-  delete uploads[id];
+  try {
+    const uploadPath = await bot.downloadFile(file_id, downloadDir);
 
-  await bot.sendMessage(
-    id,
-    `Каталог загружен и доступен по адресу https://tg.deluxspa.ru/${path.basename(
-      uploadPath
-    )}`
-  );
+    delete uploads[id];
+
+    const catalog = await fs.readFile(uploadPath);
+
+    await fs.unlink(uploadPath);
+
+    const { Contents } = await s3
+      .listObjects({
+        Bucket: bucketName,
+      })
+      .promise();
+
+    if (Contents.find(({ Key }) => Key === catalogKey)) {
+      await s3
+        .deleteObject({
+          Bucket: bucketName,
+          Key: catalogKey,
+        })
+        .promise();
+    }
+
+    const { Location } = await s3
+      .upload({
+        Body: catalog,
+        Bucket: bucketName,
+        ContentType: "application/pdf",
+        Key: catalogKey,
+      })
+      .promise();
+
+    await bot.sendMessage(
+      id,
+      `Каталог успешно загружен и доступен по адресу ${Location}`
+    );
+  } finally {
+    isBusy = false;
+  }
 });
 
 bot.on("error", (error) => {
@@ -68,6 +128,10 @@ async function main() {
       command: "/upload",
       description: "Загрузить каталог NEON BEARD",
     },
+    {
+      command: "/link",
+      description: "Получить ссылку на каталог",
+    },
   ]);
 
   if (isProduction) {
@@ -75,7 +139,12 @@ async function main() {
       await bot.closeWebHook();
     }
 
-    console.log("set webhook", await bot.setWebHook(`https://tg.deluxspa.ru/bot${process.env.TELEGRAM_API_TOKEN}`));
+    console.log(
+      "set webhook",
+      await bot.setWebHook(
+        `https://tg.deluxspa.ru/bot${process.env.TELEGRAM_API_TOKEN}`
+      )
+    );
 
     console.log("open webhook", await bot.openWebHook());
 
